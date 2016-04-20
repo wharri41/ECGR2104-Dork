aebaaa840f88985d98279a04555006ed496f55fe
wharri41 <wharri41@uncc.edu>
2016-04-19 21:23:32 -0400

    Create event.cpp

---
+++ event.cpp
@@ -0,0 +1,14 @@
+//include player and roll information
+
+void check(){
+
+  //function to call to check if there is an event at the players location
+
+}
+
+void handle(){
+
+  //find the event at that line and ask the user the question or output event
+  //change user information based on event and/or user answer to question
+
+}
